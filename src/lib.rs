3b434c96e4cfcd991ee5ecef8f292e9388f3601d
David Tolnay <dtolnay@gmail.com>
2017-04-25 16:46:11 -0700

    Merge tests into lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -21,3 +21,17 @@ impl<T, I> Reduce<T> for I where I: Iterator<Item=T> {
         self.next().map(|first| self.fold(first, f))
     }
 }
+
+#[test]
+fn test_reduce_some() {
+    let v = vec![1usize, 2, 3, 4, 5];
+    let sum = v.into_iter().reduce(|a, b| a + b);
+    assert_eq!(Some(15), sum);
+}
+
+#[test]
+fn test_reduce_none() {
+    let v = Vec::<usize>::new();
+    let sum = v.into_iter().reduce(|a, b| a + b);
+    assert_eq!(None, sum);
+}
