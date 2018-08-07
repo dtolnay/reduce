a0f65a7bfb76d2d432a20ce2ccb5716769593e26
David Tolnay <dtolnay@gmail.com>
2018-08-07 00:42:14 -0700

    Format with rustfmt 0.99.1

--- src/lib.rs
+++ src/lib.rs
@@ -8,15 +8,20 @@
 
 pub trait Reduce<T> {
     fn reduce<F>(self, f: F) -> Option<T>
-        where Self: Sized,
-              F: FnMut(T, T) -> T;
+    where
+        Self: Sized,
+        F: FnMut(T, T) -> T;
 }
 
-impl<T, I> Reduce<T> for I where I: Iterator<Item=T> {
+impl<T, I> Reduce<T> for I
+where
+    I: Iterator<Item = T>,
+{
     #[inline]
     fn reduce<F>(mut self, f: F) -> Option<T>
-        where Self: Sized,
-              F: FnMut(T, T) -> T,
+    where
+        Self: Sized,
+        F: FnMut(T, T) -> T,
     {
         self.next().map(|first| self.fold(first, f))
     }
