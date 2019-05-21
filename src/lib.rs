9d19a6e06e5a85b05d116c3bcf640234c5ee7623
David Tolnay <dtolnay@gmail.com>
2019-05-21 08:57:53 -0700

    Move tests to integration test so crate can be no_std

--- src/lib.rs
+++ src/lib.rs
@@ -20,24 +20,3 @@ where
         self.next().map(|first| self.fold(first, f))
     }
 }
-
-#[test]
-fn test_reduce_some() {
-    let v = vec![1usize, 2, 3, 4, 5];
-    let sum = v.into_iter().reduce(|a, b| a + b);
-    assert_eq!(Some(15), sum);
-}
-
-#[test]
-fn test_reduce_one() {
-    let v = vec![1usize];
-    let sum = v.into_iter().reduce(|a, b| a + b);
-    assert_eq!(Some(1), sum);
-}
-
-#[test]
-fn test_reduce_none() {
-    let v = Vec::<usize>::new();
-    let sum = v.into_iter().reduce(|a, b| a + b);
-    assert_eq!(None, sum);
-}
---
+++ tests/test.rs
@@ -0,0 +1,22 @@
+use reduce::Reduce;
+
+#[test]
+fn test_reduce_some() {
+    let v = vec![1usize, 2, 3, 4, 5];
+    let sum = v.into_iter().reduce(|a, b| a + b);
+    assert_eq!(Some(15), sum);
+}
+
+#[test]
+fn test_reduce_one() {
+    let v = vec![1usize];
+    let sum = v.into_iter().reduce(|a, b| a + b);
+    assert_eq!(Some(1), sum);
+}
+
+#[test]
+fn test_reduce_none() {
+    let v = Vec::<usize>::new();
+    let sum = v.into_iter().reduce(|a, b| a + b);
+    assert_eq!(None, sum);
+}
